4bc2356787c454517bfbf9032a928af6c19ab749
Jesse Wenger <wengerj@gmail.com>
2021-04-29 17:37:43 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,14 @@ class PostsController < ApplicationController
   end
 
   def update
+    #binding.pry
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
