2b23071e72939df998d27be40b9f02f11633805d
Jesse Wenger <wengerj@gmail.com>
2021-04-29 16:30:14 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, length: 1
+  validates :category, length: {minimum: 1}
   validates :content, length: {minimum: 100}
 end
